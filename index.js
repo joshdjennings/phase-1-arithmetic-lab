bf71e58b9723421c7d8c38e18868da899f6ac375
joshua david jennings <jennings.josh@gmail.com>
2021-07-28 17:35:47 -0500

    commit tree a8cdb90a7d59f750c41b982cce00d179c5a1ef9e

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Create a variable called `mod` set to an equation that will calculate the
+//remainder of dividing variable `num3` by `num4`; the remainder should be `4`.
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+
+//Create a variable called `max` that finds the highest number in a set; the
+//value returned should be `20`.
+
+const max = Math.max(17, 18, 19, 20);
